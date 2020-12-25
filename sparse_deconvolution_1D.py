6a9dc1e5c8be00a6948536f52cb6964e2aba11e9
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-12-25 18:38:07 +0100

    Add dirrichlet kernel

--- sparse_deconvolution_1D.py
+++ sparse_deconvolution_1D.py
@@ -22,3 +22,20 @@ def spikes_1D(x, w, p):
     assert len(w.shape) == 1
 
     return np.sum(w[p == x])
+
+
+def dirichlet_kernel(x, n):
+    """Dirichlet kernel of given order.
+
+    Args:
+    -----
+        x : float
+        n : int
+            Kernel order.
+
+    Returns:
+    --------
+        float
+
+    """
+    return np.sin((N + .5)*x)/(2*np.pi*np.sin(x/2))
