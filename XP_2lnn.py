5729fd4b4dd0c8a7e0475bc94f5c2e9d74b4dc26
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-12-30 16:34:59 +0100

    Compiling but still not working

--- XP_2lnn.py
+++ XP_2lnn.py
@@ -6,6 +6,7 @@ import two_layers_nn as tln
 import optimizer as opt
 
 
+np.random.seed(0)
 m0 = 4
 NNenv = tln.paper_env(m0, sigma_name='relu', loss_name='squared')
 
@@ -17,6 +18,12 @@ theta0 = np.concatenate((np.real(roots), np.imag(roots)), axis=1)
 
 
 bs = 5
-n_iter = 10
-gamma0 = 1e-1
-opt.SGD(NNenv, w0, theta0, bs, n_iter, gamma0)
+n_iter = 10000
+gamma0 = 1e-2
+w, theta = opt.SGD(NNenv, w0, theta0, bs, n_iter, gamma0, print_every=100)
+
+print(theta)
+
+plt.scatter(w0*theta0[:, 0], w0*theta0[:, 1], color='blue', marker='.')
+plt.scatter(w*theta[:, 0], w*theta[:, 1], color='red', marker='.')
+plt.show()
