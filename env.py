5c7652ac1a921b19f0619a9c8b77c1edf1137091
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-12-30 15:12:34 +0100

    New env for NN

--- env.py
+++ env.py
@@ -2,5 +2,33 @@
 from collections import namedtuple
 
 
-Env = namedtuple('Env', '''R phi V y g w p x_min, x_max grad_R psi psi_p
-prox_V subgrad_V lbd''')
+Env = namedtuple('Env', [
+    'R',
+    'phi',
+    'V',
+    'y',
+    'g',
+    'w',
+    'p',
+    'x_min,',
+    'x_max',
+    'grad_R',
+    'psi',
+    'psi_p',
+    'prox_V',
+    'subgrad_V',
+    'lbd',
+])
+
+NNEnv = namedtuple('NNEnv', [
+    'phi',
+    'phi_dw',
+    'phi_dtheta1',
+    'phi_dtheta2',
+    'V',
+    'y',
+    'w_bar',
+    'theta_bar',
+    'loss',
+    'loss_d1',
+])
