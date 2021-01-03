83c820374780192560945d29479811569738c616
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-03 21:52:06 +0100

    Update XP12

--- XP1-2_sd1_gaussian.py
+++ XP1-2_sd1_gaussian.py
@@ -13,7 +13,22 @@ np.random.seed(0)
 sigma = 0.1
 for m in [6, 10, 100]:
     print(f'----------m={m}----------')
-    params = parameters.XP12Params(m=m, sigma=sigma, fb_gamma=0.2)
+    params = parameters.XP12Params(m=m, sigma=sigma, fb_gamma=0.2, lbd=1e-10)
+    SD1 = sd1.SparseDeconvolution(params)
+
+    # Apply the forward backward algorithm
+    ws, thetas = opt.forward_backward(SD1, print_every=100)
+
+    # Dump arrays
+    plot.dump(ws, thetas, params)
+
+    # Plot particle flow
+    plot.plot_particle_flow_sd1(ws, thetas, params)
+
+m = 100
+for sigma in [1e-3, 1e-1, 1]:
+    print(f'----------m={m}----------')
+    params = parameters.XP12Params(m=m, sigma=sigma, fb_gamma=0.2, lbd=1e-10)
     SD1 = sd1.SparseDeconvolution(params)
 
     # Apply the forward backward algorithm
