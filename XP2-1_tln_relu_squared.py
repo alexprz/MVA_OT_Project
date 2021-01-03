890ae7fe02016194945fa22f9c71905d5c9d9fcd
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-03 22:25:54 +0100

    Add XP21

---
+++ XP2-1_tln_relu_squared.py
@@ -0,0 +1,28 @@
+"""Implement the sparse deconvolution experience of the paper."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+import parameters
+import two_layer_nn as tln
+import optimizer as opt
+import plot
+
+
+np.random.seed(0)
+
+for m in [6, 10, 100]:
+    print(f'----------m={m}----------')
+    params = parameters.XP21Params(m=m, sgd_gamma=1e-3)
+    TLN = tln.TwoLayerNN(params)
+
+    # Apply the forward backward algorithm
+    ws, thetas, *_ = opt.SGD(TLN, print_every=1000)
+
+    # Dump arrays
+    plot.dump(ws, thetas, params)
+
+    # Plot particle flow
+    plot.plot_particle_flow_sd1(ws, thetas, params)
+
+plt.tight_layout()
+plt.show()
