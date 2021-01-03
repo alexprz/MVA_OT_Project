27d887114497eb9cd035634c9360e9a44a8777c1
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-03 22:25:29 +0100

    Add parameters of TLN

--- parameters.py
+++ parameters.py
@@ -2,8 +2,10 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
-from kernels import BaseKernel, DirichletKernel, GaussianKernel
+from losses import BaseLoss, Squared
+from activations import BaseActivation, ReLU
 import sparse_deconvolution_1D as sd1
+import two_layer_nn as tln
 
 
 class BaseParameters(ABC):
@@ -210,3 +212,124 @@ class XP14Params(SD1CommonParameters):
         )
         self.name = 'XP1-4'
         self.subname = name
+
+
+class TLNParameters(BaseParameters):
+    """Store parameters for the two-layer network example."""
+
+    def __init__(self, m0, w0, theta0, w_bar, theta_bar, lbd, loss, activation,
+                 sgd_n_iter, sgd_gamma, sgd_bs):
+        """Init.
+
+        Args:
+        -----
+            m0 : int
+                Number of particles in the ground truth
+            w0 : np.array of shape (m,)
+                Initial weights
+            theta0 : np.array of shape (m,)
+                Initial positions
+            w_bar : np.array of shape (m,)
+                Groud truth weights
+            theta_bar : np.array of shape (m,)
+                Groud truth positions
+            lbd : float
+                Objective parameter
+            kernel : kernels.BaseKernel object
+            n_iter : int
+                Number of iterations of the FB algorithm
+            fb_gamma : float
+                Parameter of the FB algo
+            fb_lbd : float
+                Parameter of the FB algo
+            fb_nu : float
+                Parameter of the FB algo
+            bs : int
+                Batch size
+
+        """
+        self.m0 = m0
+        self.m = w0.shape[0]
+        self.w0 = w0
+        self.theta0 = theta0
+        self.w_bar = w_bar
+        self.theta_bar = theta_bar
+        self.lbd = lbd
+        self.loss = loss
+        self.activation = activation
+        self.sgd_n_iter = sgd_n_iter
+        self.sgd_gamma = sgd_gamma
+        self.sgd_bs = sgd_bs
+
+        self.name = None
+        self.subname = None
+
+        assert isinstance(loss, BaseLoss)
+        assert isinstance(activation, BaseActivation)
+
+    def state_dict(self):
+        """Create a dict storing parameters."""
+        return {
+            'm0': self.m0,
+            'm': self.m,
+            'lbd': self.lbd,
+            'loss': self.loss,
+            'activation': self.activation,
+            'sgd_n_iter': self.sgd_n_iter,
+            'sgd_gamma': self.sgd_gamma,
+            'sgd_bs': self.sgd_bs,
+            'which': self.subname,
+        }
+
+
+class TLNCommonParameters(TLNParameters):
+    """Store common parameters for the two-layer network example."""
+
+    def __init__(self, m, **kwargs):
+        """Init.
+
+        Args:
+        -----
+            kernel : kernels.BaseKernel object
+
+        """
+        np.random.seed(0)
+        m0 = 4
+        lbd = kwargs.get('lbd', 1/0.002)
+        w_bar, theta_bar = tln.paper2_ground_truth(m0)
+        eps = kwargs.get('eps', 1e-1)
+        w0 = eps*np.ones(m)
+        roots = np.array([np.exp(2*np.pi*1j*k/m) for k in range(m)])[:, None]
+        theta0 = np.concatenate((np.real(roots), np.imag(roots)), axis=1)
+        super().__init__(
+            m0=m0,
+            w0=kwargs.get('w0', w0),
+            theta0=kwargs.get('theta0', theta0),
+            w_bar=w_bar,
+            theta_bar=theta_bar,
+            lbd=lbd,
+            loss=kwargs.get('loss', Squared()),
+            activation=kwargs.get('activation', ReLU()),
+            sgd_n_iter=10000,
+            sgd_gamma=kwargs.get('fb_gamma', 1),
+            sgd_bs=15,
+        )
+
+
+class XP21Params(TLNCommonParameters):
+    """Implement the parameters for the experiment 2.1 (Relu + squared)."""
+
+    def __init__(self, m, **kwargs):
+        """Init.
+
+        Args:
+        -----
+            m : int
+                Number of particles in the gradient flow
+
+        """
+        super().__init__(
+            m=m,
+            **kwargs
+        )
+        self.name = 'XP2-1'
