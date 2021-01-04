3c30159d28aaf616b9eb9fbb9183872883829d07
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-04 12:33:03 +0100

    XP12b OK

--- XP1-2_sd1_gaussian.py
+++ XP1-2_sd1_gaussian.py
@@ -14,9 +14,9 @@ sigma = 0.01
 for i, m in enumerate([100, 10, 6]):
     print(f'----------m={m}----------')
     if m == 100:
-        params = parameters.XP12Params(m=m, sigma=sigma, n_iter=10000, lbd=2, fb_gamma=0.005, fb_lbd=0.01)
+        params = parameters.XP12Params(m=m, sigma=sigma, lbd=2, fb_gamma=0.005, fb_lbd=0.01)
     else:
-        params = parameters.XP12Params(m=m, sigma=sigma, n_iter=10000, lbd=2, fb_gamma=0.0001, fb_lbd=0.01)
+        params = parameters.XP12Params(m=m, sigma=sigma, lbd=2, fb_gamma=0.0001, fb_lbd=0.01)
     SD1 = sd1.SparseDeconvolution(params)
 
     # Apply the forward backward algorithm
@@ -44,20 +44,37 @@ for i, m in enumerate([100, 10, 6]):
 plt.show()
 exit()
 
+np.random.seed(0)
+w_compare, theta_compare = None, None
 m = 100
-for sigma in [1e-3, 1e-1, 1]:
+for i, sigma in enumerate([5e-3, 1e-1, 1]):
     print(f'----------m={m}----------')
-    params = parameters.XP12Params(m=m, sigma=sigma, fb_gamma=0.2, lbd=1e-10)
+    if i == 0:
+        params = parameters.XP12Params(m=m, sigma=sigma, n_iter=10000, lbd=2, fb_gamma=0.003, fb_lbd=0.01)
+    elif i == 1:
+        params = parameters.XP12Params(m=m, sigma=sigma, n_iter=10000, lbd=6, fb_gamma=0.003, fb_lbd=0.01)
+    else:
+        params = parameters.XP12Params(m=m, sigma=sigma, n_iter=10000, lbd=0.5, fb_gamma=0.003, fb_lbd=0.01)
     SD1 = sd1.SparseDeconvolution(params)
 
     # Apply the forward backward algorithm
     ws, thetas = opt.forward_backward(SD1, print_every=100)
 
+    # The first result in the one of reference
+    if w_compare is None or theta_compare is None:
+        w_compare, theta_compare = ws[-1, ...], thetas[-1, ...]
+        val_compare = sigma
+
     # Dump arrays
     plot.dump(ws, thetas, params)
 
     # Plot particle flow
-    plot.plot_particle_flow_sd1(ws, thetas, params)
+    plot.plot_particle_flow_sd1(ws, thetas, params, w_compare=None,
+                                theta_compare=None,
+                                tol_compare=1e-1,
+                                label_compare=f'Converged positions\nfor sigma={val_compare}',
+                                norm_gradient=None,  # norm_gradient,
+                                display_legend=(i == 2),
+                                )
 
-plt.tight_layout()
 plt.show()
