23c0ff686e474225547c245ebc10bc4af4893ae2
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-04 10:19:07 +0100

    Update XP11

--- XP1-1_sd1_dirichlet.py
+++ XP1-1_sd1_dirichlet.py
@@ -9,9 +9,9 @@ import plot
 
 
 np.random.seed(0)
-
+w_compare, theta_compare = None, None
 order = 7
-for m in [6, 10, 100]:
+for i, m in enumerate([100, 10, 6]):
     print(f'----------m={m}----------')
     params = parameters.XP11Params(m=m, order=order)
     SD1 = sd1.SparseDeconvolution(params)
@@ -19,11 +19,27 @@ for m in [6, 10, 100]:
     # Apply the forward backward algorithm
     ws, thetas = opt.forward_backward(SD1, print_every=100)
 
+    # The first result in the one of reference
+    if w_compare is None or theta_compare is None:
+        w_compare, theta_compare = ws[-1, ...], thetas[-1, ...]
+        val_compare = m
+
     # Dump arrays
     plot.dump(ws, thetas, params)
 
     # Plot particle flow
-    plot.plot_particle_flow_sd1(ws, thetas, params)
+    grad_w, subgrad_theta = SD1.subgrad_f_m(ws[-1, :], thetas[-1, :])
+    norm_gradient = np.linalg.norm(grad_w) + np.linalg.norm(subgrad_theta)
+    plot.plot_particle_flow_sd1(ws, thetas, params, w_compare, theta_compare,
+                                tol_compare=1e-1,
+                                label_compare=f'Converged positions\nfor m={val_compare}',
+                                norm_gradient=norm_gradient,
+                                display_legend=(i == 2),
+                                )
+
+plt.tight_layout()
+plt.show()
+exit()
 
 m = 100
 for order in [1, 5, 25]:
